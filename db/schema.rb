58f256e5285a28542577d3c374014871db32805f
keisukeadachi <c3a578zx@gmail.com>
2019-07-14 11:48:21 +0900

    add ransack

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_095525) do
+ActiveRecord::Schema.define(version: 2019_07_14_013104) do
+
+  create_table "user_searches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
